8de65f71b53e8d4fbe662a32404603af6ee4271d
Apondon <yl940830@163.com>
2017-01-06 18:13:40 +0800

    change1.6

---
+++ src/scripts/components/search.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="search">
+    <header>
+      <div>
+        <span></span>
+        <input type="text" placeholder="搜索">
+      </div>
+      <span class="goback">
+        <router-link v-bind:to="'/'">
+            取消
+        </router-link>
+      </span>
+    </header>
+  </div>
+</template>
--- src/scripts/components/user.vue
+++ src/scripts/components/user.vue
@@ -4,6 +4,48 @@
       <p>user</p>
     </header>
     <section>
+      <div></div>
+      <ul>
+        <li v-for="item in useerNav">
+          <i>{{item.ico}}</i>
+          <b>{{item.title}}</b>
+        </li>
+      </ul>
     </section>
   </div>
 </template>
+
+<script>
+module.exports = {
+  data: function () {
+    return {
+      userNav: [
+        {
+          ico: '&#xe608;',
+          title: '活动1'
+        },
+        {
+          ico: '&#xe60d;',
+          title: '活动2'
+        },
+        {
+          ico: '&#xe648;',
+          title: '活动3'
+        },
+        {
+          ico: '&#xe648;',
+          title: '活动4'
+        },
+        {
+          ico: '&#xe648;',
+          title: '活动5'
+        },
+        {
+          ico: '&#xe648;',
+          title: '活动6'
+        }
+      ]
+    }
+  }
+
+</script>
